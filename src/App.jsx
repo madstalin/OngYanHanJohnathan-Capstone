b5d060002b4f536215d9e5c104ded59c6da79e22
madstalin <heil_yanhan@hotmail.com>
2026-02-06 22:27:47 +0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [stockSymbol, setStockSymbol] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [purchasePrice, setPurchasePrice] = useState("");
+  const addStock = () => {
+    
+  }
+
+  return (
+    <>
+      <div>
+       <h1>Finance Dashboard</h1>
+      </div>
+      
+      <div className="container">
+        <input
+        type="text"
+        stockSymbol={stockSymbol}
+        onChange={(e) => setStockSymbol(e.target.stockSymbol)}
+        placeholder="Stock Symbol"
+        />
+
+      <input
+        type="text"
+        quantity={quantity}
+        onChange={(e) => setQuantity(e.target.quantity)}
+        placeholder="Quantity"
+        />
+
+       <input
+        type="text"
+        purchasePrice={purchasePrice}
+        onChange={(e) => setPurchasePrice(e.target.purchasePrice)}
+        placeholder="Purchase Price"
+        />
+
+        <button onClick={addStock}>
+          Add Stock
+        </button>
+      </div>
+      
+      
+      <p className="stocklist">
+        Stock List
+      </p>
+
+      <p className="stock">
+        No stocks added yet.
+      </p>
+    </>
+  )
+}
+
+
+
+export default App
