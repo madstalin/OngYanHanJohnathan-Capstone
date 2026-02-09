589d34969bfe715eed529426ed73a436efda1706
madstalin <heil_yanhan@hotmail.com>
2026-02-09 22:10:58 +0800

    Create ProfitLoss 2.jsx

---
+++ src/ProfitLoss 2.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+function StockCard() {
+  const stocksymbol = "AAPL";
+  const quantity = 2;
+  const purchasePrice = 123.99;
+  const currentPrice = 167.15;
+
+  const profitLoss =
+    (currentPrice - purchasePrice) * quantity;
+
+  return (
+    <div className="card">
+      <p>Symbol: {stocksymbol}</p>
+      <p>Quantity: {quantity}</p>
+      <p>Purchase Price: {purchasePrice}</p>
+      <p>Current Price: {currentPrice}</p>
+
+      <p
+        style={{
+          color: profitLoss > 0 ? "green" : "red"
+        }}
+      >
+        Profit/Loss: {profitLoss.toFixed(2)}
+      </p>
+    </div>
+  );
+}
+
+export default StockCard;
